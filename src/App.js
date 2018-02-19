c3990e47c58dfb708583a7d69408ba5f332901e5
Letitbe133 <lionel.lft@me.com>
2018-02-19 15:15:27 +0100

    Intégration material-ui et test api Tmdb

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import logo from './movie_reel.svg';
 import SearchForm from './SearchForm';
 import MovieDisplay from './MovieDisplay';
@@ -58,6 +59,21 @@ class App extends Component {
   onSubmit(e) {
     e.preventDefault() // disable default click behavior
 
+    // Tmdb API
+    // API key : api_key=039009afb934eb20795c0fe1d646eeb4
+    // Endpoint : https://api.themoviedb.org/3/
+
+    // const { query } = this.state
+    // const url = https://api.themoviedb.org/3/discover/movie?api_key=039009afb934eb20795c0fe1d646eeb4&with_keywords=${query}
+    // fetch(url)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     if (!!data.results.length) {
+    //       this.setState({details: data.results})
+    //     }
+    //   })
+    //   .catch(err => console.log('err', err))
+
     const { query } = this.state
     const url = `http://www.omdbapi.com/?apikey=e0a2c4ef&type=movie&page=2&r=json&s=${query}`
 
@@ -73,24 +89,27 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to My Flix</h1>
-        </header>
-        <h2 className="App-intro">
-          To search for a movie, just type in your query and enjoy ;).
-        </h2>
-        <SearchForm
-          onClick={this.onSubmit}
-          onChange={this.setQuery}
-        />
-        <MovieDisplay
-          query={this.state.query}
-          imdb={this.state.imdbUrl}
-          details={this.state.details}
-        />
-    </div>
+      <MuiThemeProvider>
+        <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to My Flix</h1>
+            </header>
+            {/* <h2 className="App-intro">
+              To search for a movie, just type in your query and enjoy ;).
+            </h2> */}
+            <SearchForm
+              onClick={this.onSubmit}
+              onChange={this.setQuery}
+            />
+            <MovieDisplay
+              query={this.state.query}
+              imdb={this.state.imdbUrl}
+              details={this.state.details}
+            />
+        </div>
+      </MuiThemeProvider>
+
     );
   }
 }
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,57 +1,27 @@
 import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardHeader} from 'material-ui/Card';
 
 class SearchForm extends Component {
-    // on click : get the user input
-    // call omdb api
-    // render response in MovieDisplay
-
-    // constructor(props) {
-    //     super(props);
-    //     // this.handleClick = this.handleClick.bind(this);
-    // }
-
-    // handleClick() {
-    //     const userInput = document.getElementById('userInput').value;
-    //     const target = document.getElementById('movie');
-    //     target.classList.remove('MovieDisplay-none');
-    //     target.classList.add('MovieDisplay-visible');
-
-    //     // OMDb API
-    //     // key = e0a2c4ef
-    //     // endpoint = http://www.omdbapi.com/?apikey=[yourkey]&
-    //     const url = "http://www.omdbapi.com/?apikey=e0a2c4ef&type=movie&page=2&r=json&s=" + userInput;
-    //     fetch(url)
-    //         .then((response) => response.json())
-    //         .then(function(data) {
-    //             let items = data.Search;
-    //             items.map(function(item) {
-    //                 console.log(item);
-    //             })
-    //         })
-    //         .catch(function(err) {
-    //             console.log(err);
-    //         })
-    //     this.props.onClick(userInput);
-    // }
 
     render() {
+
+        const style = {
+            margin: 50,
+        }
+
         return (
-            <div className="SearchForm">
-                <form  id="" action="#" method="get" className="SearchForm-form">
-                    <input
-                        id="userInput"
-                        className="SearchForm-form--input"
-                        placeholder="Type in your search here"
-                        onChange={e => this.props.onChange(e.target.value)}
-                    />
-                    <input
-                        type="submit"
-                        className="SearchForm-form--input---submit"
-                        value="Go & get it!"
-                        onClick={e => this.props.onClick(e)}
-                    />
-                </form>
-            </div>
+            <Card style={style}>
+                <CardHeader
+                    title="To search for a movie, just type in your query and enjoy ;)."
+                    style={style}
+                />
+                <div className="SearchForm">
+                    <TextField hintText="What are you looking for ?" onChange={e => this.props.onChange(e.target.value)}/>
+                    <RaisedButton label="Go get it Bobby!" primary onClick={e => this.props.onClick(e)}/>
+                </div>
+            </Card>
         );
     }
 }
